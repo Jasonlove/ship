f358eda8d5ce046c93e23aade9df966bbc758f7f
xiaojiajia <xiaojiajia@shouqiev.com>
2018-07-24 18:03:48 +0800

    添加基本框架

---
+++ src/main/java/com/company/ship/PersonApplication.java
@@ -0,0 +1,23 @@
+package com.company.ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Configuration
+@ComponentScan(basePackages = {"com.company"})
+//@ImportResource("classpath:applicationContext-mybatis.xml")
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableScheduling
+public class PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApplication.class, args);
+	}
+}
--- src/main/java/com/company/ship/Test.java
+++
@@ -1,5 +0,0 @@
-package com.company.ship;
-
-public class Test {
-
-}
