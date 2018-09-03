78e20663e5537cdf40cd42f48a2616615b35995b
jasonlove <861854866@qq.com>
2018-09-03 18:15:06 +0800

    测试

--- src/main/java/com/company/ship/Application.java
+++ src/main/java/com/company/ship/Application.java
@@ -18,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(Application.class, args);
 	}
 }
