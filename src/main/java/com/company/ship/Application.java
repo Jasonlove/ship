b27a5d060408afd1d2bf9199de3ad48195dc70e0
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2018-09-07 10:03:49 +0800

    我就是测试回退

--- src/main/java/com/company/ship/Application.java
+++ src/main/java/com/company/ship/Application.java
@@ -18,8 +18,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("");
-		
 		SpringApplication.run(Application.class, args);
 	}
 }
